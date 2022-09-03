454aaff911a76798be77d6a9b9e879f40b3591f1
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-09-03 21:53:14 +0000

    config for tests

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'node',
+  // roots: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/node_modules/',
+    '!src/server.ts',
+  ],
+  verbose: true,
+};
