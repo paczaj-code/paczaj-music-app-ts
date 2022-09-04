ae08f130d9ef1096231adb0c91e0aa4364276e85
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-09-04 16:30:40 +0000

    Add route for single artists

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,9 @@
-import AppController from '../controller/appController';
-import { Router } from 'express';
+import AppController from "../controller/appController";
+import { Router } from "express";
 const router = Router();
 
-router.get('/artist', AppController.getAllArtists);
-// router.get('/artist/:artistId', ApiController.getArtistData);
+router.get("/artist", AppController.getAllArtists);
+router.get("/artist/:artistId", AppController.getSingleArtist);
 // router.get('/release/:releaseId', ArtistController.getRelease);
 
 export default router;
