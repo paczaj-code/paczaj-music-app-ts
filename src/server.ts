149894f258e810e880a1205e3634b51a9a4ab7ea
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-09-04 10:54:29 +0000

    Clear code

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 // import * as path from 'path';
-import express, { Application } from 'express';
+import express, { Application } from "express";
+import router from "./router/router";
+
 // @ts-ignore
-// import front from '../build';
+import front from "../build";
 const app: Application = express();
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,26 +12,13 @@ const app: Application = express();
 //   next();
 // });
 
-// app.use(express.json());
-// const pgp = require('pg-promise')(/* options */);
-// const db = pgp(
-//   'postgres://paczajmusic:paczajmusic@paczaj-music-database:5432/paczajmusic'
-// );
+app.use(express.json());
+app.use("/api/", router);
 
-// const router = require('./src/router/router');
-// import router from './router/router';
-
-// app.use(express.json());
-// app.use('/api/', router);
-// db.any('SELECT id,name FROM artist ORDER BY name ASC')
-//     .then((data) => {
-//         console.log(JSON.stringify(data))
-//     })
-//     .catch((error) => {
-//         console.log('ERROR:', error)
-//     })
-app.get('/', (req, res) => {
-  res.send('Hello My World too sss!');
-})
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Such path does not exists",
+  });
+});
 
 export default app;
