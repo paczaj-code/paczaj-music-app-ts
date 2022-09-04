39eb302debf4579204ac06d7119dd4b1f8b15438
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-09-04 01:29:16 +0000

    Add appController

---
+++ src/controller/appController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { Artist } from '../types/types';
+import { db } from '../db/db';
+import { allArtistQuery } from '../query/query';
+
+class AppController {
+  public static async getAllArtists(
+    req: Request,
+    res: Response
+  ): Promise<Response | undefined> {
+    try {
+      const result: Artist[] = await db.any(allArtistQuery);
+
+      if (result.length === 0) {
+        return res.status(404).json('Not found');
+      }
+
+      return res.status(200).json(result);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      }
+    }
+  }
+}
+
+export default AppController;
